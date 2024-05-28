a181b9dad847d90cf213d92e53bc6412de2f074f
Berin <berin.surkovic@gmail.com>
2024-05-28 13:47:10 +0200

    Theme toggle fully implemented, bugs removed

--- Web_dev_project_2/script.js
+++ Web_dev_project_2/script.js
@@ -1,10 +1,25 @@
 // ========================================================================= //
+//  SPAPP
+// ========================================================================= //
+
+$(document).ready(function(){
+  var app = $.spapp({
+    defaultView: "#header",
+    templateDir: "views/"
+
+  });
+  app.run();
+});
+
+// ========================================================================= //
 //  Typing effect
 // ========================================================================= //
 
 var i = 0;
-var txt1 = 'Welcome to my site.'; 
+var txt1 = 'Welcome'; 
 var speed = 40;
+
+var switch_status = 0;
 
 function typeWriter1() {
   if (i < txt1.length) {
@@ -77,4 +92,56 @@ function openModal() {
   }
 }
 
+// ========================================================================= //
+//  Theme switcher
+// ========================================================================= //
+let current_theme = 'light';
+const htmlTheme = document.getElementById('themeTag');
+const themeIcon = document.getElementById('theme_button');
 
+function toggleTheme(){
+  console.log('Theme switched');
+  if (current_theme === 'dark'){
+    console.log('Theme switched to light');
+    current_theme = 'light';    
+    themeIcon.setAttribute('name', 'sunny-outline');
+    htmlTheme.setAttribute('data-theme', current_theme);
+    
+  }
+  else if (current_theme === 'light'){
+    console.log('Theme switched to dark');
+    current_theme = 'dark';
+    themeIcon.setAttribute('name', 'moon-outline');
+    htmlTheme.setAttribute('data-theme', current_theme);
+  }
+}
+
+ 
+
+  // ========================================================================= //
+  //  //NAVBAR SHOW - HIDE
+  // ========================================================================= //
+
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200 ) {
+      $("#main-nav, #main-nav-subpage").slideDown(700);
+      $("#main-nav-subpage").removeClass('subpage-nav');
+    } else {
+      $("#main-nav").slideUp(700);
+      $("#main-nav-subpage").hide();
+      $("#main-nav-subpage").addClass('subpage-nav');
+    }
+  });
+
+  // ========================================================================= //
+  //  // RESPONSIVE MENU
+  // ========================================================================= //
+
+  $('.responsive').on('click', function(e) {
+    $('.nav-menu').slideToggle();
+  });
+
+
+  
